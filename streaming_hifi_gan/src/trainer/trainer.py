615c80f6febd8a99485312a280d28fa6009c643a
Orito Itsuki <20170107+MatchaChoco010@users.noreply.github.com>
2023-01-12 10:39:17 +0900

    LogをCPUに

--- streaming_hifi_gan/src/trainer/trainer.py
+++ streaming_hifi_gan/src/trainer/trainer.py
@@ -283,7 +283,7 @@ class Trainer:
                 val_err_tot += F.l1_loss(mel, mel_g_hat).item()
 
             val_err = val_err_tot / len(self.validation_loader)
-            self.log.add_scalar("validation/mel_spec_error", val_err, self.step)
+            self.log.add_scalar("validation/mel_spec_error", val_err.item(), self.step)
 
         # 現在のckptをlatestとして保存
         self.save_ckpt()
@@ -332,7 +332,7 @@ class Trainer:
                 audio = audio.unsqueeze(0)
 
                 self.log.add_audio(
-                    f"generated/audio/{filepath.name}", audio[0], self.step, 24000
+                    f"generated/audio/{filepath.name}", audio[0].cpu(), self.step, 24000
                 )
 
                 seconds = (datetime.now() - start_time).total_seconds()
