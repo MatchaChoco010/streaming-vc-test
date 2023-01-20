bcadd6ee830d7d1135accc11a7dd6877b269e56d
Orito Itsuki <20170107+MatchaChoco010@users.noreply.github.com>
2023-01-20 13:28:39 +0900

    torchaudioを使うように

--- streaming_asr/src/data/reazon_dataset.py
+++ streaming_asr/src/data/reazon_dataset.py
@@ -1,6 +1,4 @@
-import datasets
 import pykakasi
-import torch
 import torchaudio
 from datasets import load_dataset
 from src.module.text_encoder import TextEncoder
@@ -14,18 +12,16 @@ class ReazonDataset(IterableDataset):
 
     def __init__(self, text_encoder: TextEncoder, train: bool):
         if train:
-            self.dataset = load_dataset("reazon-research/reazonspeech", streaming=True, name="all", download_config=datasets.DownloadConfig(resume_download=True))["train"].skip(600)  # type: ignore
+            self.dataset = load_dataset("reazon-research/reazonspeech", streaming=True, name="all")["train"].skip(600)  # type: ignore
         else:
-            self.dataset = load_dataset("reazon-research/reazonspeech", streaming=True, name="all", download_config=datasets.DownloadConfig(resume_download=True))["train"].take(600)  # type: ignore
+            self.dataset = load_dataset("reazon-research/reazonspeech", streaming=True, name="all")["train"].take(600)  # type: ignore
         self.kks = pykakasi.kakasi()
         self.text_encoder = text_encoder
 
     def __iter__(self):
         for data in self.dataset:
-            audio = torch.from_numpy(data["audio"]["array"]).to(dtype=torch.float32)
-            audio = torchaudio.transforms.Resample(
-                data["audio"]["sampling_rate"], 24000
-            )(audio)
+            audio, sr = torchaudio.load(data["audio"]["path"])
+            audio = torchaudio.transforms.Resample(sr, 24000)(audio)
             result = self.kks.convert(data["transcription"])
             text = "".join([item["kana"] for item in result])
             encoded_text = self.text_encoder.encode(text)
