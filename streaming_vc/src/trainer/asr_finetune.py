fc663ef8a82a7e62fb464dc952c0c217a1cf785f
Orito Itsuki <20170107+MatchaChoco010@users.noreply.github.com>
2023-01-29 06:03:52 +0900

    lr調整

--- streaming_vc/src/trainer/asr_finetune.py
+++ streaming_vc/src/trainer/asr_finetune.py
@@ -73,7 +73,7 @@ class Trainer:
 
         self.optimizer_asr_d = optim.AdamW(self.discriminator.parameters(), lr=0.001)
         self.optimizer_asr_g = optim.AdamW(
-            self.asr_model.encoder.parameters(), lr=0.00001
+            self.asr_model.encoder.parameters(), lr=0.00005
         )
 
         if exp_name is not None:
