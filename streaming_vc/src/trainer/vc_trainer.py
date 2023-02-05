2c71d05b0f06ff9a65544155859c2de665f0a0cb
Orito Itsuki <20170107+MatchaChoco010@users.noreply.github.com>
2023-02-06 05:37:59 +0900

    lr調整

--- streaming_vc/src/trainer/vc_trainer.py
+++ streaming_vc/src/trainer/vc_trainer.py
@@ -82,8 +82,8 @@ class Trainer:
         self.vocoder.load_state_dict(vocoder_ckpt["generator"])
 
         self.optimizer_spk_rm = optim.AdamW(self.spk_rm.parameters(), lr=0.0001)
-        self.optimizer_d_feat = optim.AdamW(self.d_feat.parameters(), lr=0.000001)
-        self.optimizer_d_mel = optim.AdamW(self.d_mel.parameters(), lr=0.000001)
+        self.optimizer_d_feat = optim.AdamW(self.d_feat.parameters(), lr=0.000005)
+        self.optimizer_d_mel = optim.AdamW(self.d_mel.parameters(), lr=0.000005)
         self.optimizer_mel_gen = optim.AdamW(
             itertools.chain(self.spk_rm.parameters(), self.mel_gen.parameters()),
             lr=0.0001,
@@ -249,7 +249,7 @@ class Trainer:
             xs = self.spk_rm(xs)
             xs = self.mel_gen(xs)
             xs = self.d_mel(xs)
-            spk_rm_mel_loss = F.binary_cross_entropy(xs, torch.ones_like(xs)) * 25.0
+            spk_rm_mel_loss = F.binary_cross_entropy(xs, torch.ones_like(xs)) * 10.0
             spk_rm_mel_losses.append(spk_rm_mel_loss.item())
 
             xs = self.asr_model.feature_extractor(x_many)
