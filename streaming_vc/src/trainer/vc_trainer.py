1622e57f32f0e826ddebf4a0886a799755eb8c8d
Orito Itsuki <20170107+MatchaChoco010@users.noreply.github.com>
2023-02-06 11:26:25 +0900

    lr調整

--- streaming_vc/src/trainer/vc_trainer.py
+++ streaming_vc/src/trainer/vc_trainer.py
@@ -241,7 +241,7 @@ class Trainer:
             xs = self.asr_model.encoder(xs)
             xs = self.spk_rm(xs)
             xs = self.d_feat(xs)
-            spk_rm_feat_loss = F.binary_cross_entropy(xs, torch.ones_like(xs)) * 45.0
+            spk_rm_feat_loss = F.binary_cross_entropy(xs, torch.ones_like(xs)) * 40.0
             spk_rm_feat_losses.append(spk_rm_feat_loss.item())
 
             xs = self.asr_model.feature_extractor(x_many)
@@ -249,7 +249,7 @@ class Trainer:
             xs = self.spk_rm(xs)
             xs = self.mel_gen(xs)
             xs = self.d_mel(xs)
-            spk_rm_mel_loss = F.binary_cross_entropy(xs, torch.ones_like(xs))
+            spk_rm_mel_loss = F.binary_cross_entropy(xs, torch.ones_like(xs)) * 10.0
             spk_rm_mel_losses.append(spk_rm_mel_loss.item())
 
             xs = self.asr_model.feature_extractor(x_many)
