45176bbfc5b08e85bb8c0bdeef0e3d3fbdd4cc43
Orito Itsuki <20170107+MatchaChoco010@users.noreply.github.com>
2023-02-13 08:58:53 +0900

    バッファサイズ調整

--- streaming_vc/bin/vc_train.py
+++ streaming_vc/bin/vc_train.py
@@ -7,7 +7,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 from src.trainer.vc_trainer import Trainer
 
 # default parameters
-batch_size: int = 8
+batch_size: int = 2
 max_step: int = 10000001
 progress_step: int = 10
 valid_step: int = 1000
