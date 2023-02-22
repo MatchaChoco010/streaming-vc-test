b543029eff515ec8adec19b7a8cb6eadf3c1063a
Orito Itsuki <20170107+MatchaChoco010@users.noreply.github.com>
2023-02-22 18:54:05 +0900

    bottleneckを広くしてみる

--- streaming_vc3/src/model/bottleneck.py
+++ streaming_vc3/src/model/bottleneck.py
@@ -14,10 +14,10 @@ class Bottleneck(nn.Module):
         super(Bottleneck, self).__init__()
         self.layers = nn.Sequential(
             FFTBlockST(128, 512),
-            FFTBlockST(512, 32),
+            FFTBlockST(512, 128),
         )
-        self.proj_mu = nn.Conv1d(32, 256, 1)
-        self.proj_log_sigma = nn.Conv1d(32, 256, 1)
+        self.proj_mu = nn.Conv1d(128, 256, 1)
+        self.proj_log_sigma = nn.Conv1d(128, 256, 1)
 
     def forward(self, xs: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
         xs = self.layers(xs).transpose(1, 2)
