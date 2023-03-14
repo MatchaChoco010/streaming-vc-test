389b3de0db2176e308cd3d907147085c9b00c0e2
Orito Itsuki <20170107+MatchaChoco010@users.noreply.github.com>
2023-03-14 18:22:11 +0900

    lrを修正

--- streaming_wavlm/src/trainer/trainer.py
+++ streaming_wavlm/src/trainer/trainer.py
@@ -53,7 +53,7 @@ class Trainer:
 
         self.optimizer = torch.optim.Adam(
             list(self.wavlm.parameters()) + list(self.fc.parameters()),
-            lr=0.0001,
+            lr=0.0002,
         )
 
         if exp_name is not None:
@@ -61,7 +61,7 @@ class Trainer:
 
         self.log.add_text(
             "train/params",
-            f"lr: {0.0001}  \n",
+            f"lr: {0.0002}  \n",
             0,
         )
 
